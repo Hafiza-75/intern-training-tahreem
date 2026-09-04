48823193acc424178ca40560de739f06b60b2fec
H. Tahreem Arshad <ah0690055@gmail.com>
2026-09-04 16:45:42 +0500

    Complete Day 6 React introduction

---
+++ task-dashboard/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header () {
+    return(
+        <header>
+            <h1>Task Manegment Dashboard</h1>
+            <p>Manage your tasks efficiently!</p>
+        </header>
+    );
+
+}
+
+export default Header;
---
+++ task-dashboard/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome ({name}) {
+    return (
+        <section>
+            <h1>Welcome, {name}! </h1>
+            <p>Here is an overview of tasks.</p>
+        </section>
+    );
+}
+
+export default Welcome;

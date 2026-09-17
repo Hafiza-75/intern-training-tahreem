1b08f9901cdb56d638f32088a53e1fb0be3bbf2c
H. Tahreem Arshad <ah0690055@gmail.com>
2026-09-17 13:05:30 +0500

    feat: implement DTOs and request validation for Day 18

--- day-17-nest/src/tasks/tasks.service.ts
+++ day-17-nest/src/tasks/tasks.service.ts
@@ -1,21 +1,40 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto.js';
+import { UpdateTaskDto } from './dto/update-task.dto.js';
 
 @Injectable()
 export class TasksService {
   private tasks = [
-    { id: '1', title: 'Task One', description: 'First task details' },
-    { id: '2', title: 'Task Two', description: 'Second task details' },
+    { id: 1, title: 'Learn NestJS', description: 'Understand NestJS fundamentals', completed: false },
+    { id: 2, title: 'Learn ReactJS', description: 'Understand ReactJS fundamentals', completed: true},
   ];
 
-  getAllTasks() {
+  findAll() {
     return this.tasks;
   }
 
-  getTaskById(id: string) {
+  findOne(id: number) {
     const task = this.tasks.find((t) => t.id === id);
     if (!task) {
-      throw new NotFoundException(`Task with ID "${id}" not found`);
+      throw new NotFoundException(`Task with ID ${id} not found`);
     }
+    return task;
+  }
+
+  create(createTaskDto: CreateTaskDto) {
+    const newTask = {
+      id: Date.now(),
+      title: createTaskDto.title,
+      description: createTaskDto.description,
+      completed: createTaskDto.completed ?? false,
+    };
+    this.tasks.push(newTask);
+    return newTask;
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto) {
+    const task = this.findOne(id);
+    Object.assign(task, updateTaskDto);
     return task;
   }
 }
